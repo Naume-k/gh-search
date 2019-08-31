6306e56e459783c472a33a26be1adb26dff141e2
Naume-k <kobnaome@gmail.com>
2019-08-31 10:07:46 +0200

    imported the classes and router into the form component

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-// import { Router } from "@angular/router";
+import { Router } from "@angular/router";
+import { User } from "../user";
+import { Repo } from "../repo";
 
 @Component({
   selector: 'app-search-form',
