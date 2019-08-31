e81ba3ac03b020221487aed419e04cc822d295d2
Naume-k <kobnaome@gmail.com>
2019-08-31 10:22:51 +0200

    removed the errors from the import classes

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { User } from "./user";
+import { Repo } from "./repo";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  getuser(){
+    return User
+  }
 
   constructor() { }
 }
